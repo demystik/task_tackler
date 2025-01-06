102bc985d20d19e6137f4b2cd7c47ca4ae3a2b1b
demystik <demystiktech@gmail.com>
2025-01-06 13:52:24 +0100

    new updates

---
+++ lib/practice/constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+final kAppBarTitleText = TextStyle(
+  color: Colors.white.withAlpha(200),
+  fontWeight: FontWeight.w600
+);
+
+const KTaskNumberTextStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w800,
+  fontSize: 65,
+);
+
+final KDateTextStyle = TextStyle(
+  color: Colors.white.withAlpha(200),
+  fontWeight: FontWeight.w700,
+  fontSize: 25,
+);
+final KDayTextStyle = TextStyle(
+  color: Colors.white.withAlpha(110),
+  fontWeight: FontWeight.w500,
+  fontSize: 15,
+);
+
+const KTaskTextStyle = TextStyle(
+  color: Colors.white,
+  
+);
+const KCheckedTaskTextStyle = TextStyle(
+  decoration: TextDecoration.lineThrough,
+  color: Colors.grey,
+);
--- lib/practice/list_view.dart
+++ lib/practice/list_view.dart
@@ -1,123 +1,130 @@
-// import 'package:flutter/material.dart';
-
-// class PracticeDart extends StatelessWidget {
-//   const PracticeDart({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: SafeArea(
-//         child: Scaffold(
-//           appBar: AppBar(
-//             title: const Text('Hello ListView'),
-//           ),
-//           body: Container(
-//             color: Colors.lightBlue,
-//             child: ListView.builder(
-//                 reverse: false,
-//                 scrollDirection: Axis.vertical,
-//                 itemCount: posts.length,
-//                 itemBuilder: (context, index) {
-//                   return IconList(
-//                     leadingIcon: posts[index][1],
-//                     iconName: posts[index][0],
-//                   );
-//                 }),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// List posts = [
-//   ['This is add icon', Icons.add],
-//   ['This is minus icon', Icons.remove],
-//   ['This is share icon', Icons.share],
-//   ['This is menu icon', Icons.menu],
-//   ['This is book icon', Icons.book],
-//   ['This is bus alert  icon', Icons.bus_alert],
-//   ['This is house icon', Icons.house],
-// ];
-
-// class IconList extends StatelessWidget {
-//   final IconData leadingIcon;
-//   final String iconName;
-//   const IconList(
-//       {super.key, required this.leadingIcon, required this.iconName});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.all(8),
-//       child: Container(
-//         padding: const EdgeInsets.all(8),
-//         child: ListTile(
-//           leading: Icon(leadingIcon),
-//           title: Text(iconName),
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:task_tackler/practice/constants.dart';
+
+
 
 class PracticeDart extends StatefulWidget {
   const PracticeDart({super.key});
-
   @override
   State<PracticeDart> createState() => _PracticeDartState();
 }
-
 class _PracticeDartState extends State<PracticeDart> {
-  final _controller = TextEditingController();
 
+  
+
+  final _controller = TextEditingController();
+  String postNumber = '';
+  
+
+  //Save the new task to the post 
   void onSaved() {
     setState(() {
       if (_controller.text.isNotEmpty) {
         PostList.posts.add([_controller.text, false]);
       }
       _controller.clear();
+      // showPostNumber();
     });
+    postNumber = (PostList.posts.length).toString();
     Navigator.of(context).pop();
   }
 
+  // String showPostNumber(){
+  //   setState(() {
+  //     postNumber = postNumber++;
+      
+  //   });
+  // }
+
+  //show bottom shit
   void showBottomShit(context) {
     showModalBottomSheet(
+      backgroundColor: const Color.fromARGB(255, 225, 225, 225),
       context: context,
       builder: buildBottomSheet,
     );
   }
 
+  //CheckBox Toggled
   void toggleCheckBox(bool? value, int index) {
     setState(() {
       PostList.posts[index][1] = !PostList.posts[index][1];
     });
   }
 
+  //Delete Button Confirmed
+  void deleteConfirmed(int index){
+    setState(() {
+      PostList.posts.removeAt(index);
+    });
+    postNumber = (PostList.posts.length).toString();
+    Navigator.of(context).pop();
+  }
+ 
+  //Delete button Pressed
+  void deletePressed(int index){
+    showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return  AlertDialog(
+          backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+          title: const Text('Delete this task?',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),),
+          content: const Text('This task will be removed from the list of your tasks'),
+          actions: [
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancle')
+              ),
+            OutlinedButton(
+              onPressed: () => deleteConfirmed(index),
+              child: const Text('Delete')
+              ),
+          ],
+        );
+      },
+      );
+  }
+
+
+  //Bottom sheet layout
   Widget buildBottomSheet(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
       child: Container(
+        // color: const Color.fromARGB(255, 225, 225, 225),
         child: Column(
           children: [
             const Text(
               'Add task',
-              style: TextStyle(fontSize: 30),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
             ),
+            const SizedBox(height: 15.0,),
             TextField(
+              
               controller: _controller,
               textAlign: TextAlign.center,
               autofocus: true,
               decoration: const InputDecoration(
-                border: OutlineInputBorder(),
+                hintText: 'Add new task',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1, color: Colors.grey,
+                  )
+                ),
               ),
             ),
+            const SizedBox(height: 15.0,),
             MaterialButton(
-              color: Colors.yellow,
+              
+              color: Colors.black,
               onPressed: onSaved,
-              child: const Text('Save'),
+              child: const Text('Save', style: TextStyle(color: Colors.white ),),
             )
           ],
         ),
@@ -125,76 +132,144 @@ class _PracticeDartState extends State<PracticeDart> {
     );
   }
 
+ @override
+  void initState() {
+    super.initState();
+    postNumber = PostList.posts.length.toString();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.black,
         floatingActionButton: FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 34, 33, 33),
+          shape: const CircleBorder(),
           onPressed: () => showBottomShit(context),
-          child: const Icon(Icons.add),
+          child: const Icon(Icons.add, color: Colors.white, size: 20,),
         ),
         appBar: AppBar(
-          title: const Text('ListView Test'),
-          backgroundColor: Colors.red,
-          leading: const Icon(Icons.menu),
-          actions: const [
-            Icon(Icons.smart_button),
-          ],
+          title: Center(child: Text('My To-Do List', textAlign: TextAlign.center, style: kAppBarTitleText)),
+          backgroundColor: Colors.black,
         ),
-        body: ListView.builder(
-          scrollDirection: Axis.vertical,
-          reverse: false,
-          itemCount: PostList.posts.length,
-          itemBuilder: (context, index) {
-            return ListStyle(
-              data: PostList.posts[index][0],
-              isTaskCompleted: PostList.posts[index][1],
-              toggleCheckBox: (value) {
-                toggleCheckBox(value, index);
-              },
-            );
-          },
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Container(
+                height: 80,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(postNumber, style: KTaskNumberTextStyle,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('$monthName $dayNumber', style: KDateTextStyle,),
+                          Text(dayName, style: KDayTextStyle),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  reverse: false,
+                  itemCount: PostList.posts.length,
+                  itemBuilder: (context, index) {
+                    return ListStyle(
+                      data: PostList.posts[index][0],
+                      isTaskCompleted: PostList.posts[index][1],
+                      toggleCheckBox: (value) {
+                        toggleCheckBox(value, index);
+                      },
+                      deletePressed: () { deletePressed(index); }
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
+
 class PostList {
   static List posts = [
-    ['In the name of Allah the most beneficient', false],
-    ['I love Flutter development.', false],
-    ['So bro! dart makes my heart fluter.', false],
-    ['All adorations belong to Allah', false],
+    ['Task-1', false],
+    ['Task-2', false],
+    ['Task-3', false],
+    ['Task-4', false],
   ];
+
+
 }
+
+
+
 
 // Task List Tiles
 class ListStyle extends StatelessWidget {
   final String data;
   final bool isTaskCompleted;
   final Function(bool?) toggleCheckBox;
+  final Function() deletePressed;
   const ListStyle(
       {super.key,
       required this.data,
       required this.isTaskCompleted,
-      required this.toggleCheckBox});
+      required this.toggleCheckBox,
+      required this.deletePressed,
+      });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(
-        value: isTaskCompleted,
-        onChanged: toggleCheckBox,
-      ),
-      title: Text(
-        data,
-        style: TextStyle(
-          decoration: isTaskCompleted
-              ? TextDecoration.lineThrough
-              : TextDecoration.none,
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: ListTile(
+        // tileColor: const Color.fromARGB(255, 34, 33, 33),
+        textColor:  const Color.fromARGB(197, 255, 255, 255),
+        iconColor:  const Color.fromARGB(197, 255, 255, 255),
+        leading: Transform.scale(
+          scale: 0.7,
+          child: Checkbox(
+            // visualDensity: VisualDensity(horizontal: 5.0, vertical: 5.0),
+            // shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(3)) ),
+            side: const BorderSide(width: 1, color:Color.fromARGB(255, 225, 225, 225)),
+            checkColor: Colors.white,
+            // focusColor: Colors.yellow,
+            activeColor: Colors.black,
+            // hoverColor: Colors.red,
+            value: isTaskCompleted,
+            onChanged: toggleCheckBox,
+          ),
+        ),
+        title: Text(
+          data,
+          style: isTaskCompleted? KCheckedTaskTextStyle : KTaskTextStyle,
+        ),
+        trailing:  GestureDetector(
+          onTap: deletePressed,
+          child:const  Icon(Icons.delete_rounded,size: 20, grade: 6,),
         ),
       ),
     );
   }
 }
+
+DateTime currentDate = DateTime.now();
+String dayName = DateFormat('EEEE').format(currentDate);
+String dayNumber = DateFormat('d').format(currentDate);
+String monthName = DateFormat('MMMM').format(currentDate);
